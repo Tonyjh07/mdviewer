9ae9ca37d4c36fb43ddba2cc79a38a81e74e6882
Tonyjh07 <yjh07@qq.com>
2026-07-08 19:36:45 +0800

    fix: 外链跳转默认浏览器 (tauri-plugin-opener)

--- application/src-tauri/src/lib.rs
+++ application/src-tauri/src/lib.rs
@@ -30,6 +30,7 @@ fn get_initial_file() -> Result<Option<FileData>, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![get_initial_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
